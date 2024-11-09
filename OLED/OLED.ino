588d1a5666ae77901f81ff287e8bf839e79782d6
Soumyajit-Nath <138509031+Soumyajit-Nath@users.noreply.github.com>
2024-11-10 01:05:04 +0530

    OLED

---
+++ OLED/OLED.ino
@@ -0,0 +1,59 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using I2C
+#define OLED_RESET     -1 // Reset pin
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop(){
+  // initialize the OLED object
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  // Display Text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Welcome to Agri Arena ");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Scroll full screen
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  // display.println("Full");
+  // display.println("screen");
+  // display.println("scrolling!");
+  // display.display();
+  display.startscrollright(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);    
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  display.clearDisplay();
+
+}
